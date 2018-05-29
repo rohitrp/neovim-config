36bf0771df61bb8ce358294e9351f41e0f3327e3
Rohit Prasad <rohitrp3@gmail.com>
2018-05-29 20:54:40 +0530

    Change tmux and neovim configs

--- install.sh
+++ install.sh
@@ -2,22 +2,26 @@
 install_on_my_zsh() {
   echo "Setting up zsh..." \
   && rm -rf ~/.zshrc ~/.oh-my-zsh \
+  && sudo apt-get install zsh \
   && ln -s $(pwd)/zshrc ~/.zshrc \
-  && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh \
+  && git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh \
   && chsh -s /bin/zsh \
-  && git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions \
+  && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions \
+  && git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search \
+  && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting \
   && mkdir -p ~/.oh-my-zsh/custom/themes \
   && ln -s $(pwd)/cloud_kris.zsh-theme ~/.oh-my-zsh/custom/themes \
-  && rm ~/z.sh \
+  && rm -f ~/z.sh \
   && curl -fLo ~/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
 }
 
 setup_tmux() {
   echo "Setting up tmux..." \
-  && rm -rf ~/.tmux.conf ~/.tmux \
-  && ln -s $(pwd)/tmux.conf ~/.tmux.conf \
-  && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm \
-  && ~/.tmux/plugins/tpm/bin/install_plugins
+  && sudo apt-get install tmux \
+  && rm -rf ~/.tmux.conf ~/.tmux .tmux \
+  && git clone https://github.com/gpakosz/.tmux.git \
+  && ln -s -f .tmux/.tmux.conf \
+  && cp .tmux/.tmux.conf.local ~ 
 }
 
 install_fzf() {
@@ -29,24 +33,45 @@ install_fzf() {
 
 setup_neovim() {
   echo "Setting up neovim..." \
-  && rm -rf ~/.config/nvim \
-  && git clone https://github.com/k-takata/minpac.git ~/.config/nvim/pack/minpac/opt/minpac \
-  && ln -s $(pwd)/snippets ~/.config/nvim/snippets \
-  && ln -s $(pwd)/init.vim ~/.config/nvim/init.vim \
-  && nvim -c 'packadd minpac | source $MYVIMRC | echo "Installing plugins..." | call minpac#update("", { "do": "UpdateRemotePlugins | qa!"})'
+  && sudo apt-get install software-properties-common \
+  && sudo apt-add-repository ppa:neovim-ppa/stable \
+  && sudo apt-get update \
+  && sudo apt-get install neovim \
+  && sudo apt-get install python-dev python-pip python3-dev python3-pip \
+  && rm -rf ~/.config/nvim ~/.vim \
+  && mkdir -p ~/.config \
+  && git clone https://github.com/rafi/vim-config.git ~/.config/nvim \
+  && ln -s ~/.config/nvim ~/.vim \
+  && pip install --user --upgrade PyYAML \
+  && pip install virtualenv \
+  && cd ~/.config/nvim \
+  && ./venv.sh \
+  && make test \
+  && make \
+  && rm -rf ~/.nvm \
+  && git clone https://github.com/creationix/nvm.git ~/.nvm \
+  && wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | zsh \
+  && source ~/.zshrc \
+  && nvm install node \
+  && npm -g install jshint jsxhint jsonlint stylelint sass-lint \
+  && npm -g install raml-cop markdownlint-cli write-good \
+  && pip install --user pycodestyle pyflakes flake8 vim-vint proselint yamllint \
+  && npm -g install tern \
+  && cd ~/neovim-config
 }
 
 install_fonts() {
   echo "Setting up fonts..." \
+  && mkdir -p ~/.fonts \
   && rm -rf ~/.fonts/iosevka-* \
   && cp $(pwd)/fonts/* ~/.fonts/
 }
 
 install_ripgrep() {
   echo "Installing ripgrep..." \
-  && rm -f /usr/local/bin/rg \
+  && sudo rm -f /usr/local/bin/rg \
   && curl -L https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep-0.8.1-x86_64-unknown-linux-musl.tar.gz | tar zx \
-  && cp ripgrep-0.8.1-x86_64-unknown-linux-musl/rg /usr/local/bin \
+  && sudo cp ripgrep-0.8.1-x86_64-unknown-linux-musl/rg /usr/local/bin \
   && rm -rf ripgrep-0.8.1-x86_64-unknown-linux-musl
 }
 
@@ -56,6 +81,7 @@ install_ctags() {
     echo "Installing universal ctags..." \
     && rm -rf ./ctags \
     && git clone https://github.com/universal-ctags/ctags \
+    && sudo apt-get install -y pkg-config \
     && cd ctags && ./autogen.sh && ./configure && make && sudo make install && cd ../ && rm -rf ctags
   fi
 }
@@ -76,8 +102,8 @@ if echo "$answer" | grep -iq "^y" ;then
   && sudo apt-get install urlview xdotool dh-autoreconf dconf-cli \
   && install_on_my_zsh \
   && setup_tmux \
-  && install_fzf \
   && setup_neovim \
+  && install_fzf \
   && install_fonts \
   && install_ripgrep \
   && install_ctags \
--- zshrc
+++ zshrc
@@ -3,7 +3,7 @@ ZSH=$HOME/.oh-my-zsh
 
 ZSH_THEME="cloud_kris"
 
-plugins=(git zsh-autosuggestions docker-compose z)
+plugins=(git zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting docker-compose z)
 
 source $ZSH/oh-my-zsh.sh
 . ~/z.sh
@@ -16,12 +16,12 @@ alias n='nvim .'
 alias install='sudo apt-get install'
 alias search='sudo apt-cache search'
 alias purge='sudo apt-get purge'
-alias update='sudo apt-get purge'
+alias update='sudo apt-get update'
 alias c7="sudo chmod -R 777"
 alias l="ls -l"
 alias c="clear"
 alias www="cd /var/www"
-alias code="cd ~/code"
+#alias code="cd ~/code"
 
 export PATH=$PATH:/usr/local/go/bin:~/go/bin
 export EDITOR=nvim
@@ -33,8 +33,12 @@ bindkey '^t' autosuggest-execute
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=242'
 [ -f ~/.zsh_secret ] && source ~/.zsh_secret
 
-export NVM_DIR="/home/kristijan/.nvm"
+export NVM_DIR="/home/rp/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -z "$TMUX" ] && exec tmux
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+export PATH=/home/rp/anaconda3/bin:$PATH
+
+export {http,https,ftp}_proxy="http://netmon.iitb.ac.in:80/"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
